567cd697914280dbe4fc7b7fb25abcc70376c3a9
Nicole Ridout <nicoleridout@Nicoles-MacBook-Pro.local>
2019-02-11 18:22:38 -0500

    reworked the token for auth and moved the script to return the urls

--- download_avatars.js
+++ download_avatars.js
@@ -1,26 +1,30 @@
 var request = require('request');
 var token = require("./secrets");
+var fs = require('fs');
 
 function getRepoContributors(repoOwner, repoName, cb) {
   var options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
       'User-Agent': 'request',
-      'Authorization': token.GITHUB_TOKEN
+      'Authorization': 'token ' + token.GITHUB_TOKEN
     }
   };
 
   request(options, function (err, res, body) {
-    cb(err);
     var data = JSON.parse(body);
-    var contributor = data[0];
-    data.forEach(function(contributor){
-      console.log(contributor.avatar_url);
-    });
+    cb(err, data);
   });
 }
 
 getRepoContributors("jquery", "jquery", function (err, result) {
   console.log("Errors:", err);
-  console.log("Result:", result);
+  var contributor = result[0];
+  result.forEach(function (contributor) {
+    console.log(contributor.avatar_url);
+  });
 });
+
+function downloadImageByURL(url, filePath) {
+  // ...
+}
