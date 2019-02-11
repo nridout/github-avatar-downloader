476b9ed1c1def94c4d3bc293e537dd269894b07a
Nicole Ridout <nicoleridout@Nicoles-MacBook-Pro.local>
2019-02-11 16:51:36 -0500

    added user agent request and authorization to header

--- download_avatars.js
+++ download_avatars.js
@@ -1,12 +1,18 @@
 var request = require('request');
+var request = require('./secrets.js');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request',
+      'Authorization': 'token' + GITHUB_TOKEN
+    }
+  };
 
+  request(options, function (err, res, body) {
+    cb(err, body);
+  });
 }
-
-getRepoContributors("jquery", "jquery", function (err, result) {
-  console.log("Errors:", err);
-  console.log("Result:", result);
-});
