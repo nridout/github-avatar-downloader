8e1403902d89e262eaca16afea2d4a30189decfc
Nicole Ridout <nicoleridout@Nicoles-MacBook-Pro.local>
2019-02-11 21:31:06 -0500

    added comments to completed code

--- download_avatars.js
+++ download_avatars.js
@@ -1,20 +1,42 @@
-// getRepoContributors makes a request for JSON, getting back an array of contributors.
-// getRepoContributors passes this data to cb, an anonymous callback function that it is given.
-// cb loops through each item in the array:
-// It constructs a file path using the login value(e.g., "avatars/dhh.jpg")
-// It then passes the avatar_url value and the file path to downloadImageByURL
-// downloadImageByURL fetches the desired avatar_url and saves this information to the given filePath
+// This file takes a github owner and repository as command-line arguments
+// and downloads all of the repo contributor avatar photos to the folder "avatar"
 
-
+// include required modules
 var request = require('request');
-var token = require("./secrets");
+var secret = require("./secrets");
 var fs = require('fs');
 
+// take owner and repo arguments from the command line
 var owner = process.argv[2];
 var repo = process.argv[3];
-console.log(owner, repo);
 
-//function that retrieves the repo conributors for a given repo
+// check that both owner and repo were inputted to the command line
+if (owner && repo !== undefined) {
+  // if both values valid, run getRepoContributors
+  getRepoContributors(owner, repo, function (err, result) {
+    if (!err) {
+      // takes the downloaded JSON and loops through each item in the array:
+      var contributor = result[0];
+      result.forEach(function (contributor) {
+        // constructs a file path using the login value(e.g., "avatars/dhh.jpg")
+        var filePath = './avatars/' + contributor.login + '.jpg';
+        // sets url to the contributor avatar url
+        var url = contributor.avatar_url
+        // passes the avatar_url value and the file path to downloadImageByURL
+        downloadImageByURL(url, filePath);
+      });
+    } else {
+      console.log("Errors:", err);
+    }
+  });
+} else {
+  // if not, return error
+  console.log('Error: command line argument owner or repo missing.');
+  console.log('Please specify owner and repo after download_avatars.js');
+}
+
+// getRepoContributors makes a request for JSON,
+// getting back an array of contributors
 function getRepoContributors(repoOwner, repoName, cb) {
   // creates the request options
   var options = {
@@ -22,17 +44,24 @@ function getRepoContributors(repoOwner, repoName, cb) {
     headers: {
       'User-Agent': 'request',
       // uses the token from the secrets file
-      'Authorization': 'token ' + token.GITHUB_TOKEN
+      'Authorization': 'token ' + secret.GITHUB_TOKEN
     }
   };
-  // reqests the api data and returns in JSON
+  // passes request option, checking for errors & status code
   request(options, function (err, res, body) {
-    var data = JSON.parse(body);
-    cb(err, data);
+    // if no errors & status code is 'ok'
+    if (!err && res.statusCode === 200) {
+      // parse the returned array of contributors to JSON
+      var data = JSON.parse(body);
+      // pass error report & JSON to callback
+      cb(err, data);
+    } else {
+      console.log('Data not found');
+    }
   });
 }
 
-//downloads the avatar images using the url
+// downloadImageByURL fetches the desired avatar_url
 function downloadImageByURL(url, filePath) {
   request.get(url)
     .on('error', function (err) {
@@ -41,22 +70,9 @@ function downloadImageByURL(url, filePath) {
     .on('response', function (response) {
       console.log('Downloading image...');
     })
+    // saves this information to the given filePath
     .pipe(fs.createWriteStream(filePath))
     .on('close', function () {
       console.log('Download complete.');
     });
 }
-
-// calls the getRepoContributors function, passing in a repo owner & name
-// logs errors
-// logs the avatar urls for each contributor
-getRepoContributors(owner, repo, function (err, result) {
-  console.log("Errors:", err);
-  var contributor = result[0];
-  result.forEach(function (contributor) {
-    var filePath = './avatars/' + contributor.login + '.jpg';
-    var url = contributor.avatar_url
-    downloadImageByURL(url, filePath);
-  });
-});
-
